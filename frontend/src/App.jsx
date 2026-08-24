9b74d436db1eca5415cd588e2709efdaef59c936
karamchandsuresh <karamchandsuresh@gmail.com>
2026-08-25 00:45:09 +0530

    Connect frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "http://127.0.0.1:5000/analyze";
+const API_URL = "https://cricket-shot-classification.onrender.com/analyze";
 
 const MODEL_STATS = [
   {
